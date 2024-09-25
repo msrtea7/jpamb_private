8fa9fa6a6ad20686d42181cc4296b56bac750405
Anders Vedel <s163932@student.dtu.dk>
2024-09-25 21:55:56 +0200

    Added random tester, that generates random input

--- solutions/interpret.py
+++ solutions/interpret.py
@@ -3,10 +3,10 @@
 """
 
 from dataclasses import dataclass
-import sys, logging
+import sys, logging, random, string
 from typing import Optional
 
-from jpamb_utils import InputParser, IntValue, MethodId
+from jpamb_utils import InputParser, IntValue, MethodId, CharValue, BoolValue, IntListValue, CharListValue
 
 l = logging
 l.basicConfig(level=logging.DEBUG, format="%(message)s")
@@ -110,10 +110,66 @@ class SimpleInterpreter:
 
 
 
+##################################### RANDOM TESTER ####################################
+
+
+def generate_random_input():
+    JVM_TYPES = ["int", "boolean", "char","int[]", "char[]"]
+    jvm_type = random.choice(JVM_TYPES)
+
+    if jvm_type == "int":
+        return IntValue(random.randint(0, 100000000000)) 
+    
+
+    if jvm_type ==  "char":
+       return CharValue(random.choice(string.ascii_letters))
+    
+    if jvm_type == "boolean":
+         return BoolValue(random.choice([True, False]))
+    
+    if jvm_type == "int[]":
+        array_size = random.randint(1, 10)
+        return IntListValue(tuple(random.randint(-100, 100) for _ in range(array_size))) 
+ 
+
+    if jvm_type == "char[]":
+        array_size = random.randint(1, 10)
+        return CharListValue(tuple(random.choice(string.ascii_letters) for _ in range(array_size)))
+   
+    raise ValueError(f"Unsupported JVM type: {jvm_type}")
+
+def run_random_tests(method_id_str, num_tests=10, depth_limit=10):
+    methodid = MethodId.parse(method_id_str)
+    m = methodid.load()
+
+    for i in range(num_tests):
+        random_input = generate_random_input()  # Example for int type input
+        inputs = [random_input]
+
+        interpreter = SimpleInterpreter(m["code"]["bytecode"], [input.tolocal() for input in inputs], [])
+
+        print(f"Test {i + 1}: Input={inputs}")
+        result = interpreter.interpet(limit=depth_limit)
+        print(f"Result: {result}")
+
+        if interpreter.done == "out of time":
+            print("Test timed out.")
+        elif "Exception" in result:
+            print(f"Exception thrown: {result}")
+        else:
+            print(f"Execution finished normally: {result}")
+
+
+
 
 if __name__ == "__main__":
-    methodid = MethodId.parse(sys.argv[1])
-    inputs = InputParser.parse(sys.argv[2])
-    m = methodid.load()
-    i = SimpleInterpreter(m["code"]["bytecode"], [i.tolocal() for i in inputs], [])
-    print(i.interpet())
+
+    if len(sys.argv) > 2:
+            methodid = MethodId.parse(sys.argv[1])
+            inputs = InputParser.parse(sys.argv[2])
+            m = methodid.load()
+            i = SimpleInterpreter(m["code"]["bytecode"], [i.tolocal() for i in inputs], [])
+            print(i.interpet())
+
+    else:
+        run_random_tests("jpamb.cases.Simple.justReturn:()I", num_tests=10, depth_limit=10)
